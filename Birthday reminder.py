b319a2f89f90483ac39948acb0d93a23382e79a7
Diksha-chaware <75140112+Diksha-chaware@users.noreply.github.com>
2020-11-28 02:36:31 +0530

    Birthday -reminder file  created

---
+++ Birthday reminder.py
@@ -0,0 +1,7 @@
+#time module is must reminder
+#is set with the help of dates
+import time
+#os module is used to notify the user 
+import os
+#birthday file is a file in which month and date is present
+birthdayFile = ' /path/to/birthday/:
