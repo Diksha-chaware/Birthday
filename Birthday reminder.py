5263df5a476a94654106900e638c8432f2adf506
Diksha-chaware <75140112+Diksha-chaware@users.noreply.github.com>
2020-11-30 00:46:20 +0530

    complete birthday-reminder.py

--- Birthday reminder.py
+++ Birthday reminder.py
@@ -8,3 +8,13 @@ birthdayFile = ' /path/to/birthday/:
 def checkTodaysBirthday ():
   fileName = open(birthdayFile,today = time. strftime( '%m%d' )
                   flag = 0
+                  for line in fileName :
+                  if today in line :
+                  line = line.split(' ')
+                  flag =1
+                  #line[1] contains Name
+                  os.system('notify-send + ' ' + line[2] + '"' )
+                            if flag ==0 :
+                            os.system('notify-send if __name__ == '__main__' :)
+                                      checkTodaysBirthday()
+
